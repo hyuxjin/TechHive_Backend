766f23697e8760a22a245cd0d7b7471870758669
Rodlyn Mahilum <123444084+RodlynSilent@users.noreply.github.com>
2024-10-14 16:08:45 +0800

    Revert "Migrate Admin Backend Files 2"
    This reverts commit bfff41ba0e0dccca0b10e1dbc4149bc497cd7c27.

--- src/main/java/com/example/admin_backend/Repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.admin_backend.Repository;
-
-import com.example.admin_backend.Entity.ProfileEntity;
-import com.example.admin_backend.Entity.AdminEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfileRepository extends JpaRepository<ProfileEntity, Integer> {
-    ProfileEntity findByAdmin(AdminEntity admin);
-}
